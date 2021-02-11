b0460eec0b7f6eb4652f1900bae0bede545c7696
lambert-ike-1232 <57732695+lambert-ike-1232@users.noreply.github.com>
2021-02-11 17:31:58 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,7 +4,7 @@ import unittest
 class TestDecideRespons(unittest.TestCase):
 
     def test_build(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
